56285e6228bf8a499eb8767ee1229e2c5fb490cc
Sekilleran <SebastianHD_Diaz1@outlook.com>
2024-05-31 21:02:37 -0300

    React-Router[React-Store]

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+export default function Home () {
+    return (
+        <>
+            <div className="title">
+                <h1>
+                    Hola bienvenidos
+                </h1>
+
+            </div>
+        </>
+    )
+}
